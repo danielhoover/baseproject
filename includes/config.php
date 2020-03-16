adad8d4a82d3a96b1ac864554e2bdc34271db35f
Daniel Hoover <spam@daniel-hoover.de>
2020-03-16 11:53:17 +0100

    changes for bootstrap4

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,9 @@
 <?php
 
-define('URL_PATH', '/kufstein/baseproject');
+//if it is located under http://localhost/baseproject
+//you need to change it to define('URL_PATH', '/baseproject');
+
+define('URL_PATH', '');
 
 define('LOGIN_URL', 'login');
 define('LOGOUT_URL', 'logout');
--- includes/views/parts/header.php
+++ includes/views/parts/header.php
@@ -7,16 +7,16 @@
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
 	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+
 	<?php if($this->current == "login"): ?>
 		<link href="css/toastr.min.css" rel="stylesheet">
 	<?php endif; ?>
 
-		<link href="css/main.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
 
 
-		<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
 
 	<?php if($this->current == "index"): ?>
 		<script type="text/javascript" src="js/core.js"></script>
